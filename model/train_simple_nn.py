f7a504a39cec92ce50d5fe2d3ca8c9446cfcd9ef
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-22 14:51:59 +0000

    Adds file structure

---
+++ model/train_simple_nn.py
@@ -0,0 +1,17 @@
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+# import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import pickle
+import cv2
+import os

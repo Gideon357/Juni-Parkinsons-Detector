283f18c48aba1d39bde79414e3e91f6e029c6efb
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-25 20:19:10 -0400

    Add training to server

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from flask import Flask, render_template, request
+from PIL import Image
+from time import sleep
 import json
+import random
 
 app = Flask("app")
 
-
 @app.route("/")
 def hello_world():
-	if request.user_agent.browser == "msie":
-		return render_template("html/msie.html")
-	else:
-		return render_template("html/index.html")
+	return render_template("index.html")
 
 
 @app.route("/survey")
 def survey():
-	return render_template("js/survey.js")
+	return render_template("survey.js")
 
 
 @app.route("/model_code")
 def model_code_nb():
-	return render_template("html/model_notebook.html")
+	return render_template("model_notebook.html")
 
 
 @app.route('/results')
 def results():
-	raw_result = request.args.get('results')
-	result = json.loads(raw_result)
-	return result
+	return f"<h1>It Works!</h1>"
 
 app.run(host="0.0.0.0", port=8080, debug=True)
